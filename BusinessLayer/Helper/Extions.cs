38e7816095a5947575e2ad98b1fd370d6171a910
harunayyildiz <harunayyildizz15@gmail.com>
2021-12-09 00:30:19 +0300

    🗃️ Example Extensions

---
+++ BusinessLayer/Helper/Extions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Helper
+{
+    public static class Extions
+    {
+        public static string formattedDate(DateTime date)
+        {
+            return ((DateTime)date).ToString("dd-MMM-yyyy");
+        }
+    }
+}
